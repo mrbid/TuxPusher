979c6bd5e0e2cd53932027fc8b4104657e3926ff
James William Fletcher <78346668+mrbid@users.noreply.github.com>
2023-07-05 23:33:47 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1417,10 +1417,15 @@ int main(int argc, char** argv)
         SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
         SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
         wnd = SDL_CreateWindow(appTitle, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, winw, winh, SDL_WINDOW_RESIZABLE | SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
-        if(wnd == NULL)
+        while(wnd == NULL)
         {
-            printf("ERROR: SDL_CreateWindow(): %s\n", SDL_GetError());
-            return 1;
+            if(msaa == 0)
+            {
+                printf("ERROR: SDL_CreateWindow(): %s\n", SDL_GetError());
+                return 1;
+            }
+            SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, msaa/2);
+            wnd = SDL_CreateWindow(appTitle, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, winw, winh, SDL_WINDOW_RESIZABLE | SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
         }
         SDL_GL_SetSwapInterval(1); // 0 for immediate updates, 1 for updates synchronized with the vertical retrace, -1 for adaptive vsync
         glc = SDL_GL_CreateContext(wnd);
