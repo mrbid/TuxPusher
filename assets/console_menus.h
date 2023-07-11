b26bc5aeecdcacc8fa14b73aed8ac047cacd8181
Jm15itch <86093097+Jm15itch@users.noreply.github.com>
2023-07-11 13:19:45 +0000

    Added console_menus.h back
    accidentally placed this file inside of the root directory instead of under assets

---
+++ assets/console_menus.h
@@ -0,0 +1,51 @@
+#ifndef HelpMenu 
+#define HelpMenu \
+"\n" \
+"     =====================================================\n" \
+"     |                                                   |\n" \
+"     |                  TuxPusher.com                    |\n" \
+"     |                                                   |\n" \
+"     |     James William Fletcher (github.com/mrbid)     |\n" \
+"     |                                                   |\n" \
+"     =====================================================\n\n" \
+"\n" \
+"  -= ARGUMENTS =-\n\n" \
+"Access this menu\n" \
+"    --help\n" \
+"    -h\n\n" \
+"Change MSAA Levels (1, 2, 4, 8, 16)\n" \
+"    --msaa-level {VALUE}\n" \
+"    --msaa {VALUE}\n\n" \
+"Change VSYNC Settings (-1, 0, 1)\n" \
+"    --vertical-sync {OPTION}\n" \
+"    --vs {OPTION}\n\n" \
+"Change Game Speed Settings (1-32)\n" \
+"    --push-speed {VALUE}\n" \
+"    --ps {VALUE}\n\n" \
+"\n" \
+"  -= CONTROLS =-\n\n" \
+"Left Click = Release coin\n" \
+"Right Click = Show/hide cursor\n" \
+"C = Orthographic/Perspective\n" \
+"F = Print the FPS to the console\n" \
+"\n\n" \
+"  -= CREDITS =-\n\n" \
+"Tux 3D model by Andy Cuccaro:\n" \
+"https://sketchfab.com/3d-models/tux-157de95fa4014050a969a8361a83d366\n" \
+"Fonts used in the End Game screen from top to bottom order:\n" \
+"1. Fontasy Penguin by FontasyLand\n" \
+"   https://www.fontspace.com/fontasy-penguin-font-f4848\n" \
+"2. Plastic Love by Azkarizki\n" \
+"   https://www.fontspace.com/plastic-love-font-f49676\n" \
+"3. Allay Font by Abahrozi (https://twinletter.com/)\n" \
+"   https://www.fontspace.com/allay-font-f66225\n" \
+"\n\n" \
+"  -= CONTRIBUTORS =-\n\n" \
+"Jm15itch (github.com/Jm15itch)\n\n" \
+"\n" \
+"  -= RULES =-\n\n" \
+"- A gold coin in a silver slot rewards you 2x silver coins.\n" \
+"- A gold coin in a gold slot rewards you 2x gold coins.\n" \
+"- A tux in a slot when you already have the tux gives you 6x gold coins and 6x silver coins.\n" \
+"\n\n"
+#endif
