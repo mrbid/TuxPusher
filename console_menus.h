e62651ceea729aa0afdc0f0b34d2b20e351fbd6c
Jm15itch <86093097+Jm15itch@users.noreply.github.com>
2023-07-11 12:39:09 +0000

    Seperated menu strings.
    So main.c looked less cluttered.

---
+++ console_menus.h
@@ -0,0 +1,32 @@
+#ifndef HelpMenu 
+#define HelpMenu \
+"----\n" \
+"TuxPusher.com\n" \
+"----\n" \
+"James William Fletcher (github.com/mrbid)\n" \
+"----\n" \
+"Argv(2): msaa, speed\n" \
+"e.g; ./uc 16 1.6\n" \
+"----\n" \
+"Left Click = Release coin\n" \
+"Right Click = Show/hide cursor\n" \
+"C = Orthographic/Perspective\n" \
+"F = FPS to console\n" \
+"----\n" \
+"Tux 3D model by Andy Cuccaro:\n" \
+"https://sketchfab.com/3d-models/tux-157de95fa4014050a969a8361a83d366\n" \
+"----\n" \
+"Fonts used in the End Game screen from top to bottom order:\n" \
+"1. Fontasy Penguin by FontasyLand\n" \
+"   https://www.fontspace.com/fontasy-penguin-font-f4848\n" \
+"2. Plastic Love by Azkarizki\n" \
+"   https://www.fontspace.com/plastic-love-font-f49676\n" \
+"3. Allay Font by Abahrozi (https://twinletter.com/)\n" \
+"   https://www.fontspace.com/allay-font-f66225\n" \
+"----\n" \
+"Rules:\n" \
+"Getting a gold coin in a silver slot rewards you 2x silver coins.\n" \
+"Getting a gold coin in a gold slot rewards you 2x gold coins.\n" \
+"Getting a tux in a slot when you already have the tux gives you 6x gold coins and 6x silver coins.\n" \
+"----\n"
+#endif
